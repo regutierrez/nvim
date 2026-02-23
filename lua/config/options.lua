b12da449ffab827909190f1c9a50b19bd5ffa4c6
pael@noco <rpegutierrez@gmail.com>
2026-02-23 08:35:15 -0500

    add reload keymap; fix copy not going to clipboard on non-mac machines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+vim.g.lazyvim_python_lsp = "basedpyright"
+
+if vim.fn.has("mac") ~= 1 then
+  vim.o.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -5,6 +5,8 @@ return {
       sources = {
         explorer = {
           hidden = true,
+          -- exempt specific directories from being ignored (even if in .gitignore)
+          include = { "pael", ".agents" },
         },
       },
     },
